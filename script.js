320b243d28e102ff0c120a13f890797928154246
kws413 <ks600146@wne.edu>
2024-04-18 14:39:39 -0400

    create grid implement (hardcode)

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 function main() {
 
+    function createGrid(gridDimensions) {
+        const container = document.querySelector("grid-container");
+        for (var i=0; i < gridDimensions; i++) {
+            const newColumn = document.createElement("newColumn" + i);
+            newColumn.style.display = "flex";
+            for (var x=0; x < gridDimensions; x++) {
+                const newSquare = document.createElement("newSquare" + i);
+                setSquareProperties(newSquare);
+                newColumn.appendChild(newSquare);
+            }
+            container.appendChild(newColumn);
+        }
+    }
+
+    function setSquareProperties(square) {
+        square.style.backgroundColor = "pink";
+        square.style.borderStyle = "solid";
+        square.style.height = "2em";
+        square.style.width = "2em";
+    }
+
+
+    const button = document.querySelector("#makeGrid");
+    button.addEventListener("click", function (e) {
+        createGrid(16);
+    });
+    
 }
 
-main;
+main();
