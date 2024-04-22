2802e3f988713dd7cc56cd6f11e809ddab331ecc
kws413 <ks600146@wne.edu>
2024-04-22 11:26:12 -0400

    rework + add fill container growth

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function main() {
 
+    /*
     function createGrid(gridDimensions) {
-        const container = document.querySelector("grid-container");
+        const container = document.querySelector(".grid-container");
         for (var i=0; i < gridDimensions; i++) {
             const newColumn = document.createElement("newColumn" + i);
             newColumn.style.display = "flex";
@@ -21,12 +22,25 @@ function main() {
         square.style.width = "2em";
     }
 
-
     const button = document.querySelector("#makeGrid");
     button.addEventListener("click", function (e) {
         createGrid(16);
     });
-    
+    */
+
+    function createGrid(size) {
+        const container = document.querySelector(".grid-container");
+        for (var row = 0; row < size; row++) {
+            const docrow = document.createElement("row" + row);
+            docrow.id = "row";
+            container.appendChild(docrow);
+            for (var i = 0; i < size; i++) {
+                const square = document.createElement("square");
+                docrow.appendChild(square);
+            }
+        }
+    }
+    createGrid(2);
 }
 
 main();
