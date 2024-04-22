0e5401f334c8da310630baaea208961d02471b5d
kws413 <ks600146@wne.edu>
2024-04-22 11:39:54 -0400

    hover functionality / need reset

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
 function main() {
 
-    /*
-    function createGrid(gridDimensions) {
-        const container = document.querySelector(".grid-container");
-        for (var i=0; i < gridDimensions; i++) {
-            const newColumn = document.createElement("newColumn" + i);
-            newColumn.style.display = "flex";
-            for (var x=0; x < gridDimensions; x++) {
-                const newSquare = document.createElement("newSquare" + i);
-                setSquareProperties(newSquare);
-                newColumn.appendChild(newSquare);
-            }
-            container.appendChild(newColumn);
-        }
-    }
-
-    function setSquareProperties(square) {
-        square.style.backgroundColor = "pink";
-        square.style.borderStyle = "solid";
-        square.style.height = "2em";
-        square.style.width = "2em";
-    }
-
-    const button = document.querySelector("#makeGrid");
-    button.addEventListener("click", function (e) {
-        createGrid(16);
-    });
-    */
-
     function createGrid(size) {
         const container = document.querySelector(".grid-container");
         for (var row = 0; row < size; row++) {
@@ -37,10 +9,18 @@ function main() {
             for (var i = 0; i < size; i++) {
                 const square = document.createElement("square");
                 docrow.appendChild(square);
+                square.addEventListener("mouseover", function (e) {
+                    square.style.backgroundColor = "gray";
+                });
             }
         }
     }
-    createGrid(2);
+
+
+    const button = document.querySelector("#makeGrid");
+    button.addEventListener("click", function (e) {
+        createGrid(16);
+    });
 }
 
 main();
